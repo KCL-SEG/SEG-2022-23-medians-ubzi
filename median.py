e44f0df5998ed6c4efcbd6f3b172fdae56342c27
Ubaydullah Naik <ubaydullah.naik@kcl.ac.uk>
2022-10-07 11:18:43 +0100

    Add function to find median

--- median.py
+++ median.py
@@ -9,4 +9,16 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+def findMedian(numbers):
+    numbers.sort()
+    length = len(numbers)
+    if (length%2 == 1):
+        median = numbers[int(length/2)]
+    else:
+        temp = int(length//2)
+        median = (numbers[temp] + numbers[temp+1]) / 2
+    return median
+
 print(numbers)
+print(findMedian(numbers))
